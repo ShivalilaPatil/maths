4c54e04feae7d108357033ce7781ced734689f44
ShivalilaPatil <shivalilapatil66@gmail.com>
2022-02-21 01:01:20 +0530

    added division opr

---
+++ divisi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int name ();
+{
+    int no1, no2;
+    no1= 20;
+    no2= 5;
+    cout<<"\nno1= "<<no1;
+    cout<<"\nno2= "<<no2;
+    cout<<"\n division= "<<no1/no2;
+}
